86f478319fefab30641bc59ab656ebc039a569ba
patdemko <patdemko@users.noreply.github.com>
2018-11-16 11:42:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod +x /home/$USER/natest/tests.sh
+mkdir /home/$USER/natest/data
 crontab -l > mycron
 echo "*/5 * * * * /home/$USER/natest/tests.sh > /dev/null" >> mycron
 crontab mycron

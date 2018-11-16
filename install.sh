ad65c877c13b879e0e640d4bc5da37137684c30d
patdemko <patdemko@users.noreply.github.com>
2018-11-16 10:51:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 apt-get update
 apt-get upgrade
 apt-get install iperf3
+chrmod +x /home/$USER/natest/tests.sh
 crontab -l > mycron
 echo "*/5 * * * * /home/$USER/natest/tests.sh > /dev/null" >> mycron
 crontab mycron
